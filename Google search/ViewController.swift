882f1ad367385beaf5be641c05e57062eb7f2466
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-04-26 05:48:44 +0800

    Initial Commit

---
+++ Google search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google search
+//
+//  Created by 簡吟真 on 2021/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
